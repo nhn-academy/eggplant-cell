ba0d801ccc3e496951921df5affea3973f39fd5f
김보민 <qnt012@naver.com>
2022-08-24 10:05:25 +0900

    [chore🏗] : 개발 환경설정

---
+++ src/main/java/com/nhnacademy/eggplantcell/EggplantCellApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.eggplantcell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggplantCellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EggplantCellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/eggplantcell/EggplantCellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.eggplantcell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggplantCellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
